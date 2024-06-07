e43f436b208f84961a5a007f5f9abe9b9e239cb5
SammyOwO <Cowflop84@gmail.com>
2024-06-07 11:27:03 -0400

    Made stuff more organized.

--- CMDRPG/Program.cs
+++ CMDRPG/Program.cs
@@ -7,9 +7,9 @@ using Newtonsoft.Json;
 public class Game
 {
     private static System.Timers.Timer skillTimer;
-    public static class Globals
+    public class Data
     {
-        public static string saveFileOutline = @"{
+        static string saveFileOutline = @"{
         'Name': '',
         //hp, strength, defense, mana, crit chance, crit damage, hp regen
         'Stats': [100, 25, 0, 50, 20, 150, 25],
@@ -24,18 +24,51 @@ public class Game
         //is the player new or not, duhh
         'New': true
         }";
-    }
-    public class Save
-    {
+        public static SaveFile saveData = JsonConvert.DeserializeObject<SaveFile>(saveFileOutline);
         public static void save()
         {
-            SaveFile saveData = JsonConvert.DeserializeObject<SaveFile>(Globals.saveFileOutline);
+            Data.saveFileOutline = JsonConvert.SerializeObject(saveData);
             if (Directory.Exists(@"./Saves/") == false)
             {
                 Directory.CreateDirectory(@"./Saves/");
             }
             string fullPath = @".\Saves\" + saveData.Name + ".json";
-            File.WriteAllText(fullPath, Globals.saveFileOutline);
+            File.WriteAllText(fullPath, Data.saveFileOutline);
+        }
+        public static void load()
+        {
+            Console.Clear();
+            string saveDir = @".\Saves\";
+            var saveList = Directory.EnumerateFiles(saveDir);
+            Console.WriteLine("Select a save to continue:");
+            foreach (string saves in saveList)
+            {
+                Console.WriteLine(Path.GetFileNameWithoutExtension(saves));
+            }
+            Console.WriteLine();
+            while (true)
+            {
+                string saveFile = Console.ReadLine();
+                bool valid = File.Exists(saveDir + saveFile + ".json");
+                if (valid == true)
+                {
+                    Console.Clear();
+                    using (StreamReader r = new StreamReader(saveDir + saveFile + ".json"))
+                    {
+                        string loadFile = r.ReadToEnd();
+                        SaveFile loadFromJson = JsonConvert.DeserializeObject<SaveFile>(loadFile);
+                        saveFileOutline = JsonConvert.SerializeObject(loadFromJson);
+                    }
+                    Welcome();
+                }
+                else
+                {
+                    Console.WriteLine("\n" + saveFile + ".json is not a valid file.");
+                    Console.WriteLine("Please try again. \n");
+                    continue;
+                }
+                break;
+            }
         }
     }
     public class SaveFile
@@ -63,7 +96,6 @@ public class Game
     }
     public static void Main()
     {
-        SaveFile saveData = JsonConvert.DeserializeObject<SaveFile>(Globals.saveFileOutline);
         SetTimer();
         skillTimer.Enabled = false;
         Console.WriteLine("Hewwo :3 \nPress any key to continue ^w^");
@@ -83,8 +115,8 @@ public class Game
                     Console.WriteLine("First, let's get started by making a name for yourself! \nType your name below to continue:");
                     string name = Console.ReadLine();
                     Console.Clear();
-                    saveData.Name = name;
-                    Console.Write("Hi " + saveData.Name + "! \nWelcome to CMD RPG \nPress any key!");
+                    Data.saveData.Name = name;
+                    Console.Write("Hi " + Data.saveData.Name + "! \nWelcome to CMD RPG \nPress any key!");
                     Console.ReadKey();
                     Console.Clear();
                     Console.WriteLine("In this game you have 7 main stats. HP or Hit/health Points, Strength which determines your damage, Defense for how much incoming damage you block and Mana for magic weapons. \nThen there is Crit Chance for how likely you are to score a critical hit and Crit Damage for how much more damage that hit does. \nLast but not least there is HP regen for how much HP you get back per turn.");
@@ -95,44 +127,12 @@ public class Game
                     Console.WriteLine("When you get higher skill levels you'll be able to use better tools and such. \nNow when you get to the first screen there might look like there's a lot to do, which there is, but it's not as scary as it seems. \nThere will be a few main buttons for your inventory and such, but it should all be pretty intuitive.");
                     Console.WriteLine("Now, when you get in, your first objective will be to get some wood to craft your first tools! \nKind of reminds me of some other game, but I'm not sure, hehe :3 \nPress any key to continue to the main attraction!");
                     Console.ReadKey();
-                    Globals.saveFileOutline = JsonConvert.SerializeObject(saveData);
-                    Save.save();
+                    Data.save();
                     Tutorial();
                     break;
 
                 case "Load":
-                    Console.Clear();
-                    string saveDir = @".\Saves\";
-                    var saveList = Directory.EnumerateFiles(saveDir);
-                    Console.WriteLine("Select a save to continue:");
-                    foreach (string saves in saveList)
-                    {
-                        Console.WriteLine(Path.GetFileNameWithoutExtension(saves));
-                    }
-                    Console.WriteLine();
-                    while (true)
-                    {
-                        string saveFile = Console.ReadLine();
-                        bool valid = File.Exists(saveDir + saveFile + ".json");
-                        if (valid == true)
-                        {
-                            Console.Clear();
-                            using (StreamReader r = new StreamReader(saveDir + saveFile + ".json"))
-                            {
-                                string loadFile = r.ReadToEnd();
-                                SaveFile loadFromJson = JsonConvert.DeserializeObject<SaveFile>(loadFile);
-                                Globals.saveFileOutline = JsonConvert.SerializeObject(loadFromJson);
-                            }
-                            Welcome();
-                        }
-                        else
-                        {
-                            Console.WriteLine("\n" + saveFile + ".json is not a valid file.");
-                            Console.WriteLine("Please try again. \n");
-                            continue;
-                        }
-                        break;
-                    }
+                    Data.load();
                     break;
                 default:
                     Console.Clear();
@@ -144,10 +144,9 @@ public class Game
     }
     public static void Welcome()
     {
-        SaveFile saveData = JsonConvert.DeserializeObject<SaveFile>(Globals.saveFileOutline);
-        Console.WriteLine("Welcome back " + saveData.Name + "! \nPress any key to continue UwU");
+        Console.WriteLine("Welcome back " + Data.saveData.Name + "! \nPress any key to continue UwU");
         Console.ReadKey();
-        bool newPlayer = saveData.New;
+        bool newPlayer = Data.saveData.New;
         if (newPlayer == true)
         {
             while (true)
@@ -182,12 +181,103 @@ public class Game
     public static void Tutorial()
     {
         Console.Clear();
-        Console.WriteLine("");
+        Console.WriteLine("Welcome to the tutorial. \nThe first task is to get wood and craft your first set of tools. \nPress any key to continue.");
         Console.ReadKey();
+        Console.Clear();
+        while (true)
+        {
+            Console.WriteLine("Here you will see a mock up of the main menu to get you used to the main controls.");
+            Console.WriteLine("After that you can explore around for yourself. For now go to places and go to the woods in the village to chop some trees. \n");
+            Console.WriteLine("Main Menu: \n");
+            Console.WriteLine("P: Places \nI: Inventory \nA: Achievements \nX: Exit \n");
+            string next = Console.ReadLine();
+            switch (next)
+            {
+                case "p": case "P":
+                    
+                    while(true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Where would you like to go? \n \nPlaces:");
+                        Console.WriteLine("1. Village (Lvl 0-5) \n2. Caves (Lvl 5-15) \n3. Mines (Lvl 15-25) \n4. Mountains (Lvl 25-40) \n");
+                        Console.WriteLine("Type the number of where you would like to go below:");
+                        string place = Console.ReadLine();
+                        switch (place)
+                        {
+                            case "1":
+                                Console.Clear();
+                                Console.WriteLine(); break;
+                            case "2": 
+                                Console.Clear();
+                                Console.WriteLine("Not unlocked yet! Level is too low (Also it's the still tutorial silly). \n"); continue;
+                            case "3":
+                                Console.Clear();
+                                Console.WriteLine("Not unlocked yet! Level is too low (Also it's the still tutorial silly). \n"); continue;
+                            case "4":
+                                Console.Clear();
+                                Console.WriteLine("Not unlocked yet! Level is too low (Also it's the still tutorial silly). \n"); continue;
+                        }
+                        break;
+                    }
+                    break;
+                case "i": case "I":
+                    Console.Clear();
+                    Console.WriteLine("Wait until after the tutorial goober. :3 \n"); continue;
+                case "a": case "A":
+                    Console.Clear();
+                    Console.WriteLine("Wait until after the tutorial goober. :3 \n"); continue;
+                case "x": case "X":
+                    Exit(); break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Not a valid key, try again. \n");
+                    continue;
+            }
+            break;
+        }
     }
     public static void MenuMain()
     {
         Console.Clear();
-        Console.WriteLine("");
+        Console.WriteLine("Main Menu: \n");
+        Console.WriteLine("P: Places \nI: Inventory \nA: Achievements \nX: Exit");
+        string next = Console.ReadLine();
+        while (true)
+        {
+            switch(next)
+            {
+                case "p": case "P":
+                    MenuPlaces(); break;
+                case "i": case "I":
+                    MenuInventory(); break;
+                case "a": case "A":
+                    MenuAchievements(); break;
+                case "x": case "X":
+                    Exit(); break;
+                default:
+                    Console.WriteLine("Not a valid key, try again.");
+                    continue;
+            }
+            break;
+        }
+    }
+    public static void MenuPlaces()
+    {
+
+    }
+    public static void MenuInventory()
+    {
+
+    }
+    public static void MenuAchievements()
+    {
+
+    }
+    public static void Exit()
+    {
+        Console.Clear();
+        Data.save();
+        Console.WriteLine("Game saved! Press any key to exit. :3");
+        Console.ReadKey();
     }
 }
